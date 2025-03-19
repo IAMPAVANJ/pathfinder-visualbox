ec8eadbd6c0c9def68d98556e2f284817edca880
IAMPAVANJ <pavanjadhav45710@gmail.com>
2025-03-19 08:54:02 +0530

    logo chnages

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -99,7 +99,7 @@ export default function Nav(
   return (
     <div className="flex items-center justify-center min-h-[4.5rem] border-b shadow-gray-600 sm:px-5 px-0">
       <div className="flex items-center lg:justify-between justify-center w-full sm:w-[52rem]">
-        <img src="/linkedInProfile.jpeg" className="rounded-full size-8 cursor-pointer border-2"/>
+        <img src="/pathfinder.svg" className="rounded-full size-8 cursor-pointer border-2" title="click me" onClick={()=>window.open("https://www.linkedin.com/in/iampavan/","_blank")}/>
         <h1 className="lg:flex hidden w-[40%] text-2xl pl-1 text-white">
           Path Finder
         </h1>
